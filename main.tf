1ed24c1f52bcfbe8a6ec28d9eb94b01bcdeb5ec7
Taylor Nation <tnation14@gmail.com>
2017-04-06 16:12:23 -0400

    Add support for S3 bucket CORS configuration

--- main.tf
+++ main.tf
@@ -27,6 +27,14 @@ resource "aws_s3_bucket" "site_bucket" {
   policy = "${data.aws_iam_policy_document.read_only_bucket_policy.json}"
   region = "${var.region}"
 
+  cors_rule {
+    allowed_headers = "${var.cors_allowed_headers}"
+    allowed_methods = "${var.cors_allowed_methods}"
+    allowed_origins = "${var.cors_allowed_origins}"
+    expose_headers  = "${var.cors_expose_headers}"
+    max_age_seconds = "${var.cors_max_age_seconds}"
+  }
+
   tags {
     Project     = "${var.project}"
     Environment = "${var.environment}"
--- variables.tf
+++ variables.tf
@@ -1,6 +1,26 @@
 variable "bucket_name" {}
 variable "logs_bucket_name" {}
 
+variable "cors_allowed_headers" {
+  default = ["Authorization"]
+}
+
+variable "cors_allowed_methods" {
+  default = ["GET"]
+}
+
+variable "cors_allowed_origins" {
+  default = ["*"]
+}
+
+variable "cors_expose_headers" {
+  default = []
+}
+
+variable "cors_max_age_seconds" {
+  default = "3000"
+}
+
 variable "region" {
   default = "us-east-1"
 }
